85dc607d990cbf929ba45d254d35a668cc830f7b
Chi-Han Huang <huangchihan@gmail.com>
2021-06-10 13:54:00 +0800

    Gather ext data structures to one file.

--- internal/app/models/data_structs.go
+++ internal/app/models/data_structs.go
@@ -22,3 +22,12 @@ type InquiryChatRoom struct {
 	ExpiredAt     time.Time      `json:"expired_at"`
 	CreatedAt     time.Time      `json:"created_at"`
 }
+
+type CompleteChatroomInfoModel struct {
+	Chatroom
+	ServiceType   ServiceType   `json:"service_type"`
+	InquiryStatus InquiryStatus `json:"inquiry_status"`
+	InquiryUuid   string        `json:"inquiry_uuid"`
+	InquirerId    int           `json:"inquirer_id"`
+	PickerId      int           `json:"picker_id"`
+}
