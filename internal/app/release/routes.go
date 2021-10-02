199f95587e1c0e29b635c60e37d68014d4835e45
Chi-Han Huang <huangchihan@gmail.com>
2021-10-03 00:17:39 +0800

    Added CORS middlewares.

--- internal/app/release/routes.go
+++ internal/app/release/routes.go
@@ -2,10 +2,11 @@ package release
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/huangc28/go-darkpanda-backend/internal/app/middlewares"
 )
 
 func Routes(r *gin.RouterGroup) {
 	g := r.Group("/release")
 
-	g.GET("/android/latest", AndroidLatestDLLinkHandler)
+	g.GET("/android/latest", middlewares.CORSMiddlewares(), AndroidLatestDLLinkHandler)
 }
